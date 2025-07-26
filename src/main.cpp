ffda218730fcc8c1e9a609aa835aed215e1f1c0d
Mystical433 <peterpanlee2015@gmail.com>
2025-07-27 00:21:47 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,16 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PauseLayer.hpp>
-
 using namespace geode::prelude;
 
 class $modify(SettingsOnPauseMenuPleaseWork, PauseLayer) {
     void customSetup() override {
         PauseLayer::customSetup();
-
         auto sprite = CCSprite::createWithSpriteFrameName("GJ_optionsBtn02_001.png");
         sprite->setScale(0.79f);
-
         auto button = CCMenuItemSpriteExtra::create(
             sprite, this, menu_selector(SettingsOnPauseMenuPleaseWork::onOptions)
         );
         button->setID("main-options"_spr);
-
         if (auto leftButtonMenu = this->getChildByID("left-button-menu")) {
             if (auto menu = typeinfo_cast<CCMenu*>(leftButtonMenu)) {
                 menu->addChild(button);
@@ -22,8 +18,9 @@ class $modify(SettingsOnPauseMenuPleaseWork, PauseLayer) {
             }
         }
     }
-
+    
     void onOptions(CCObject*) {
-        this->showOptions();
+        auto options = OptionsLayer::create();
+        options->showLayer(false);
     }
 };
