434cded5be0f7bea2636ed05f0ece5dacc8e0322
Mystical433 <peterpanlee2015@gmail.com>
2025-07-27 17:17:03 +0900

    ass

--- src/ui/ui.hpp
+++ src/ui/ui.hpp
@@ -1,15 +1,18 @@
 #pragma once
+
 #include <Geode/Geode.hpp>
+
 using namespace geode::prelude;
+
 enum SettingPage {
     Gameplay,
     Practice,
     Performance,
     Audio,
     Misc,
-    Keybinds,
-    Credits // i forgor
+    Keybinds
 };
+
 enum SettingCellType {
     Default,
     FMODDebug,
@@ -17,6 +20,7 @@ enum SettingCellType {
     SongOffset,
     Separator
 };
+
 using SearchCB = std::function<void(std::string)>;
 class SearchPopup : public geode::Popup<SearchCB> {
 protected:
@@ -27,17 +31,40 @@ protected:
 public:
     static SearchPopup* create(SearchCB callback);
 };
+
 class SettingCell : public CCNode {
 protected:
     CCMenuItemToggler* m_toggler;
+
     bool init(std::string name, std::string gv, SettingCellType type);
     void onCheckboxToggled(CCObject* sender);
+    void onFMODDebug(CCObject*);
     void onSongSelect(CCObject*);
-    void onFMODDebug(CCObject*);  // Added missing declaration
     void onInfo(CCObject*);
 public:
     std::string m_name;
     std::string m_gameVariable;
     SettingCellType m_type;
     static SettingCell* create(std::string name, std::string gv, SettingCellType type = SettingCellType::Default);
+};
+
+class SettingsLayer : public geode::Popup<> {
+protected:
+    CCArray* m_listItems;
+    Border* m_border;
+    CCMenuItemSpriteExtra* m_currentBtn;
+    CCMenuItemSpriteExtra* m_searchClearBtn;
+
+    bool setup() override;
+    void createSettingCheckbox(
+        std::string name,
+        std::string gv
+    );
+    void switchPage(SettingPage, bool, CCMenuItemSpriteExtra*);
+    void onCategoryBtn(CCObject*);
+    void onSearchBtn(CCObject*);
+    void onClearSearch(CCObject*);
+    void refreshList();
+public:
+    static SettingsLayer* create();
 };
